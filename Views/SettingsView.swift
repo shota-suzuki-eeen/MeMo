d503c172d696ba86ae414d967179d7c6e321526d
shota suzuki <pop.eeen.iin@gmail.com>
2026-06-17 02:03:24 +0900

    ライブアクティビティ機能の説明文の調整

--- Views/SettingsView.swift
+++ Views/SettingsView.swift
@@ -30,7 +30,7 @@ struct SettingsView: View {
     private let developerPinCode = "eeen"
     private let hiddenTapRequiredCount = 15
 
-    private let appVersion = "1.0.4"
+    private let appVersion = "1.1.0"
     private let contactURL = URL(
         string: "https://docs.google.com/forms/d/e/1FAIpQLScpk7wVSUGvr8AA2RDpVa3gak2lA_wk0GbLeQTlI62Wc0X58g/viewform?usp=header"
     )!
